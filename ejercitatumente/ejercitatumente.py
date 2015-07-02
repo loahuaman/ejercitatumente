f00dbda92954f8cf2ce20349bc462c8630e7dc41
loahuaman <edithloacordova@gmail.com>
2015-07-02 11:48:26 -0500

    mi segundo commit

--- ejercitatumente/ejercitatumente.py
+++ ejercitatumente/ejercitatumente.py
@@ -30,11 +30,16 @@ def main():
     pantalla=pygame.display.set_mode((500,400))
     
     pygame.display.set_caption("Mi Ventana") # Titulo de la Ventana
+    #-----para que la ventana se quede hasta cerrar
+    salir=False  
+    while salir!=True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:# pygame.QUIT( para que cierre cruz de la ventana) 
+                salir = True
     pygame.display.update() #actualizo el display
     pygame.quit()
     quit()
 
  
-if __name__ == '__main__':
 
-   main()
+main()
