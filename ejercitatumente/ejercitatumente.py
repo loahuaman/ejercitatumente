fe9f2fa0c337d66b70b4ed83a779028f9ac6bcea
loahuaman <edithloacordova@gmail.com>
2015-07-07 20:51:58 -0500

    mi cuarto commit con un fondo de ventana

--- ejercitatumente/ejercitatumente.py
+++ ejercitatumente/ejercitatumente.py
@@ -26,11 +26,11 @@ verde=(52,170,70)
  
 def main():
     pygame.init() # inicializo el modulo
-    # fijo las dimensiones de la pantalla a 500,400 
-    pantalla=pygame.display.set_mode((500,400))
-    
+    # fijo las dimensiones de la pantalla a 800,400 
+    pantalla=pygame.display.set_mode((800,600))
+    imagenfondo=pygame.image.load("mar.jpg").convert_alpha()#capturando una imagen de fondo
     pygame.display.set_caption("Mi Ventana") # Titulo de la Ventana
-    pantalla.fill(verde) #el color de la ventana
+    pantalla.blit(imagenfondo,(0,0)) 
     pygame.display.update() #actualizo el display
     #-----para que la ventana se quede hasta cerrar
     salir=False  
