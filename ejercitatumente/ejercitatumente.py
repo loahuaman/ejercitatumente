2888423b3d3db952c4c5a78fb686c86c2535cad3
loahuaman <edithloacordova@gmail.com>
2015-07-27 15:50:26 -0500

    commit formando la imagen de mi juego

--- ejercitatumente/ejercitatumente.py
+++ ejercitatumente/ejercitatumente.py
@@ -37,33 +37,66 @@ class Boton(pygame.sprite.Sprite):
  
 # Funciones
 
-# ---------------------------------------------------------------------
+def load_image(filename, transparent=True):
+
+        try: image = pygame.image.load(filename)
+
+        except pygame.error, message:
+
+                raise SystemExit, message
+
+        image = image.convert()
+
+        if transparent:
+
+                color = image.get_at((0,0))
+
+                image.set_colorkey(color, RLEACCEL)
+
+        return image
 
  
-
 # ---------------------------------------------------------------------
 
  
 def main():
     pygame.init() # inicializo el modulo
-    # fijo las dimensiones de la pantalla a 800,400 
+    # fijo las dimensiones de la pantalla a 800,600 
     pantalla=pygame.display.set_mode((WIDTH,HEIGHT))
     pygame.display.set_caption("juego ejercita tu mente") # Titulo de la Ventana
     imagenfondo=pygame.image.load("mar.jpg").convert()#capturando una imagen de fondo
-    boton=pygame.image.load("boton.png").convert_alpha()
+    background_image = load_image('concha.png')
     pantalla.blit(imagenfondo,(0,0))
-    botonr=Boton(boton,80,100)
+    
+    
+    
+    x=80 #se dan las pociciones en el eje x
+    x1=270 
+    x2=460 
+    x3=80 
+    x4=270 
+    x5=460 
+    
+    boton1=Boton(background_image,x,100)  #se crean los botones
+    boton2=Boton(background_image,x1,100) 
+    boton3=Boton(background_image,x2,100)
+    boton4=Boton(background_image,x3,300) 
+    boton5=Boton(background_image,x4,300)
+    boton6=Boton(background_image,x5,300)
+
     cursor1=Cursor()
-    
-    
-    #-----para que la ventana se quede hasta cerrar
     salir=False  
     while salir!=True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:# pygame.QUIT( para que cierre cruz de la ventana) 
                 salir = True
         cursor1.update() #se actualiza el cuadro invisible que actua como boton del cursor
-        botonr.update(pantalla, cursor1) #se actualiza cada boton al acercar el cursor
+        boton1.update(pantalla, cursor1) #se actualiza cada boton al acercar el cursor
+        boton2.update(pantalla, cursor1)
+        boton3.update(pantalla, cursor1)
+        boton4.update(pantalla, cursor1)
+        boton5.update(pantalla, cursor1)
+        boton6.update(pantalla, cursor1)
         pygame.display.update() #actualizo el display
     pygame.quit()
     
